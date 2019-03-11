dbc758a2087131e1c1bb14f978973b2779a06611
Naskiran <nasir.kiran@live.fr>
2019-03-11 10:47:55 +0100

    Bases JS

---
+++ hello-wilder.js
@@ -0,0 +1,6 @@
+const movie = "Armageddon";
+const year = "1998";
+const real = "Micheal Bay"
+const myMessage = "My favourite film is " + movie + ", it was realised by " + real + " in " + year;
+
+alert(myMessage);
